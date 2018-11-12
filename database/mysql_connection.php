c1c8c27aa55b50c7d250ec03955f7bf159ccfc2d
erdaye <2225274057@qq.com>
2018-11-12 02:01:42 -0800

    add an egs about database connection more

--- database/mysql_connection.php
+++ database/mysql_connection.php
@@ -4,9 +4,19 @@
 
 	if ($link) {
 		echo "connection success!<br />";
-		
-		if(mysqli_select_db($link,"mysql")){
+		$database = "mysql";
+		if(mysqli_select_db($link,$database)){
 			echo "connection database mysql success!<br />";
+			$query = "select * from user;";
+			$res = mysqli_query($link,$query);
+
+			while ($row = mysqli_fetch_assoc($res)) {
+				echo $row['Host']."<br />";
+				echo $row['User']."<br />";
+				echo $row['Select_priv']."<br />";
+				echo $row['Insert_priv']."<br />";
+			}
+
 		}
 
 	}else{
